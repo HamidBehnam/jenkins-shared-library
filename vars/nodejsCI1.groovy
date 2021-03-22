536d5103d5cbb2f5c18dbfb959cc36ac0ac6147e
HamidBehnam <hamid.behnam@gmail.com>
2021-03-21 19:26:28 -0700

    params test

--- vars/nodejsCI1.groovy
+++ vars/nodejsCI1.groovy
@@ -44,8 +44,7 @@ def call(body) {
                     MY_DOMAIN_NAME = """${sh(
                             returnStdout: true,
                             script: '''
-                            my_domain=${DOMAIN_NAME_TEST}
-                            [  -z "$my_domain" ] && echo "Empty: Yes" || echo ${DOMAIN_NAME_TEST}'''
+                            [  -z ${DOMAIN_NAME_TEST} ] && echo "Empty: Yes" || echo ${DOMAIN_NAME_TEST}'''
                     ).trim()}"""
                 }
                 steps {
