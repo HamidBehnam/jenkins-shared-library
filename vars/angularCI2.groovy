4c42c7426d174d63ab324d3e4b2dfaf413f3f4ca
HamidBehnam <hamid.behnam@gmail.com>
2021-06-28 10:05:37 -0700

    angularCI2, install dependencies before running the tests

--- vars/angularCI2.groovy
+++ vars/angularCI2.groovy
@@ -83,7 +83,7 @@ def call(body) {
                     sh '''
                     rm -rf node_modules
                     npm install
-                    npm run test:ci
+                    npm run ng test -- --watch=false
                     '''
                 }
             }
