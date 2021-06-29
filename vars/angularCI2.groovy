fc16481f5a17ad6890b4e50d7d61ef145118c11c
HamidBehnam <hamid.behnam@gmail.com>
2021-06-28 17:38:00 -0700

    angularCI2, install dependencies before running the tests

--- vars/angularCI2.groovy
+++ vars/angularCI2.groovy
@@ -81,7 +81,8 @@ def call(body) {
             stage('Unit Tests') {
                 steps {
                     sh '''
-                    sudo apt-get update
+                    cd /usr/lib/node_modules/puppeteer
+                    ls
                     npm run test:ci
                     '''
                 }
