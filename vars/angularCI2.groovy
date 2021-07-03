680176e892f7b30974bb7745776396d2873243fc
HamidBehnam <hamid.behnam@gmail.com>
2021-07-03 16:19:45 -0700

    working on the angularCI2 pipeline to add the testing stage

--- vars/angularCI2.groovy
+++ vars/angularCI2.groovy
@@ -12,7 +12,9 @@ def call(body) {
     pipeline {
         agent {
             docker {
-                image 'hamidbehnam:docker-sample-img-001'
+                image 'hamidbehnam\n' +
+                        '/\n' +
+                        'docker-sample-img-001'
             }
 
         }
