ce8b2669c88687326e1965c5e6746aa9fd7a7d5e
HamidBehnam <hamid.behnam@gmail.com>
2021-07-03 14:24:03 -0700

    working on the angularCI2 pipeline to add the testing stage

--- vars/angularCI2.groovy
+++ vars/angularCI2.groovy
@@ -34,111 +34,119 @@ def call(body) {
                 }
             }
 
-            stage('Pre Build') {
-                parallel {
-                    stage('Print Info') {
-                        steps {
-                            sh '''
-                            node --version
-                            ls
-                            '''
-                        }
-                    }
+//            stage('Pre Build') {
+//                parallel {
+//                    stage('Print Info') {
+//                        steps {
+//                            sh '''
+//                            node --version
+//                            ls
+//                            '''
+//                        }
+//                    }
+//
+//                    stage('Clearing') {
+//                        steps {
+//                            sh """
+//                            rm -rf node_modules
+//                            rm -rf ${destProjectTempDirectory}
+//                            rm -rf dist
+//                            """
+//                        }
+//                    }
+//                }
+//            }
+//
+//            stage('Dependencies Installation') {
+//                steps {
+//                    sh 'npm install'
+//                }
+//            }
+//
+//            stage('Build') {
+//                steps {
+//                    sh '''
+//                    if [ ${BRANCH_NAME} = "master" ]
+//                    then
+//                    npm run build -- --configuration=production --base-href /${PROJECT_PATH}
+//                    elif [ ${BRANCH_NAME} = "qa" ]
+//                    then
+//                    npm run build -- --configuration=qa --base-href /${PROJECT_PATH}
+//                    else
+//                    npm run build -- --configuration=dev --base-href /${PROJECT_PATH}
+//                    fi'''
+//                }
+//            }
 
-                    stage('Clearing') {
-                        steps {
-                            sh """
-                            rm -rf node_modules
-                            rm -rf ${destProjectTempDirectory}
-                            rm -rf dist
-                            """
-                        }
-                    }
-                }
-            }
-
-            stage('Dependencies Installation') {
-                steps {
-                    sh 'npm install'
-                }
-            }
-
-            stage('Build') {
+            stage('Unit Tests') {
                 steps {
                     sh '''
-                    if [ ${BRANCH_NAME} = "master" ]
-                    then
-                    npm run build -- --configuration=production --base-href /${PROJECT_PATH}
-                    elif [ ${BRANCH_NAME} = "qa" ]
-                    then
-                    npm run build -- --configuration=qa --base-href /${PROJECT_PATH}
-                    else
-                    npm run build -- --configuration=dev --base-href /${PROJECT_PATH}
-                    fi'''
+                    echo "this is for test"
+                    '''
                 }
             }
 
-            stage('Push to Dest Repo') {
-                when {
-                    anyOf {
-                        branch 'dev';
-                        branch 'qa';
-                        branch 'master';
-                    }
-                }
-                steps {
-                    sh """
-                    cd dist
-                    ls
-                    mkdir ${destProjectTempDirectory}
-                    cd ${destProjectTempDirectory}
-                    #make sure the repository does have the related branch. you might need to manually create all the branches needed for the jenkins like dev, qa.
-                    git clone --single-branch --branch ${BRANCH_NAME} https://${DEST_REPO} .
-                    rm -rf *
-                    cp -a ../${SRC_PROJECT_NAME}/. .
-                    git config user.name "${GITHUB_CRED_USR}"
-                    git config user.email "${GITHUB_USER_EMAIL}"
-                    git add .
-                    git diff --quiet && git diff --staged --quiet || git commit -am "adding the build files to the dest repo"
-                    git push https://${GITHUB_CRED_USR}:${GITHUB_CRED_PSW}@${DEST_REPO}
-                    """
-                }
-            }
-
-            stage('Deployment') {
-                environment {
-                    TARGET_DOMAIN_NAME = """${sh(
-                            returnStdout: true,
-                            script: '''
-                            if [ ${DOMAIN_NAME} = 'default' ]; then echo ${DEFAULT_DOMAIN_NAME}; else echo ${DOMAIN_NAME}; fi'''
-                    ).trim()}"""
-                }
-                when {
-                    anyOf {
-                        branch 'dev';
-                        branch 'qa';
-                        branch 'master';
-                    }
-                }
-                steps {
-                    script {
-                        step([$class: "RundeckNotifier",
-                              includeRundeckLogs: true,
-                              jobId: "${RUNDECK_JOB_ID}",
-                              rundeckInstance: "${RUNDECK_INSTANCE_NAME}",
-                              options: """
-                                  src_project_name=${SRC_PROJECT_NAME}
-                                  project_path=${PROJECT_PATH}
-                                  deployment_branch=${BRANCH_NAME}
-                                  dest_repo=${DEST_REPO}
-                                  domain_name=${TARGET_DOMAIN_NAME}
-                                  """,
-                              shouldFailTheBuild: true,
-                              shouldWaitForRundeckJob: true,
-                              tailLog: true])
-                    }
-                }
-            }
+//            stage('Push to Dest Repo') {
+//                when {
+//                    anyOf {
+//                        branch 'dev';
+//                        branch 'qa';
+//                        branch 'master';
+//                    }
+//                }
+//                steps {
+//                    sh """
+//                    cd dist
+//                    ls
+//                    mkdir ${destProjectTempDirectory}
+//                    cd ${destProjectTempDirectory}
+//                    #make sure the repository does have the related branch. you might need to manually create all the branches needed for the jenkins like dev, qa.
+//                    git clone --single-branch --branch ${BRANCH_NAME} https://${DEST_REPO} .
+//                    rm -rf *
+//                    cp -a ../${SRC_PROJECT_NAME}/. .
+//                    git config user.name "${GITHUB_CRED_USR}"
+//                    git config user.email "${GITHUB_USER_EMAIL}"
+//                    git add .
+//                    git diff --quiet && git diff --staged --quiet || git commit -am "adding the build files to the dest repo"
+//                    git push https://${GITHUB_CRED_USR}:${GITHUB_CRED_PSW}@${DEST_REPO}
+//                    """
+//                }
+//            }
+//
+//            stage('Deployment') {
+//                environment {
+//                    TARGET_DOMAIN_NAME = """${sh(
+//                            returnStdout: true,
+//                            script: '''
+//                            if [ ${DOMAIN_NAME} = 'default' ]; then echo ${DEFAULT_DOMAIN_NAME}; else echo ${DOMAIN_NAME}; fi'''
+//                    ).trim()}"""
+//                }
+//                when {
+//                    anyOf {
+//                        branch 'dev';
+//                        branch 'qa';
+//                        branch 'master';
+//                    }
+//                }
+//                steps {
+//                    script {
+//                        step([$class: "RundeckNotifier",
+//                              includeRundeckLogs: true,
+//                              jobId: "${RUNDECK_JOB_ID}",
+//                              rundeckInstance: "${RUNDECK_INSTANCE_NAME}",
+//                              options: """
+//                                  src_project_name=${SRC_PROJECT_NAME}
+//                                  project_path=${PROJECT_PATH}
+//                                  deployment_branch=${BRANCH_NAME}
+//                                  dest_repo=${DEST_REPO}
+//                                  domain_name=${TARGET_DOMAIN_NAME}
+//                                  """,
+//                              shouldFailTheBuild: true,
+//                              shouldWaitForRundeckJob: true,
+//                              tailLog: true])
+//                    }
+//                }
+//            }
 
             stage('Post Build') {
                 steps {
